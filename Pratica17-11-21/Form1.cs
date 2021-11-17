58a2406918b3a1a39cefcc4fed4159d504db0093
Andy Bernal <andybernal101@gmail.com>
2021-11-17 13:52:53 -0600

    aplicacion C# finalizada

---
+++ Pratica17-11-21/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pratica17_11_21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double Num1 = Convert.ToDouble(txtNum1.Text);
+            double Num2 = Convert.ToDouble(txtNum2.Text);
+
+
+            double result = Num1 + Num2;
+
+            txtResultado.Text = Convert.ToString(result);
+        }
+    }
+}
